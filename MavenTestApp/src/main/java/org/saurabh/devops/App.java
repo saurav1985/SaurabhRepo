41dddd7326c76e953bf044ae59b3a3e57589eac9
saurabhs2 <saurabhsharmapune@gmail.com>
2015-07-29 15:21:14 +0530

    maven

---
+++ MavenTestApp/src/main/java/org/saurabh/devops/App.java
@@ -0,0 +1,13 @@
+package org.saurabh.devops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
